b5cf0e68170fb15d1516134036ed1ddf68f673c1
Omer Bitikcioglu <o.bitikcioglu@gmail.com>
2022-05-07 12:18:05 +0300

    Gereksinim 3- Javadoc Düzenlemesi

--- src/omer/bitikcioglu/homework/tests/CoffeeMachineTest.java
+++ src/omer/bitikcioglu/homework/tests/CoffeeMachineTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Add some description here.
+ * Test Class for CoffeeMachineTest class.
  *
  * @author Ömer Faruk Bitikçioğlu
  */
